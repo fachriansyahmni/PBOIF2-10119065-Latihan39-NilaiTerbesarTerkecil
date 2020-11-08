49f23ccba08648bb3cfa591086ddccb40370d6f7
fachriansyahmni <fachriansyahmni@gmail.com>
2020-11-08 08:17:34 +0700

    update

--- src/pboif2/pkg10119065/latihan39/nilaiterbesarterkecil/Mahasiswa.java
+++ src/pboif2/pkg10119065/latihan39/nilaiterbesarterkecil/Mahasiswa.java
@@ -1,8 +1,12 @@
 package pboif2.pkg10119065.latihan39.nilaiterbesarterkecil;
 
-/**
- *
- * @author Fachriansyah PC
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan nilai terkecil dan terbesar
+                          dengan objek
  */
 public class Mahasiswa {
     private int nilai;
